21aaaee995afc032f2c9e6c6b7548a890bf87b35
David Pham <davidpham5@gmail.com>
2023-02-06 14:44:40 -0500

    add vite to project

---
+++ src/index.js
@@ -0,0 +1,1 @@
+export { DataPrivacyComponent } from './DataPrivacyComponent.js';
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      entry: 'src/index.js',
+      format: ['es'],
+      name: 'DataPrivacyComponent',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['lit'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          lit: 'lit',
+        },
+      },
+    },
+  },
+});
